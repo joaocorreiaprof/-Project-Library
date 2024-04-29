b23622d0e23b8fc0aabae700c584236fd9c475d6
João Correia <joaocorreiaprof@gmail.com>
2024-04-29 22:00:10 +0100

    Implementing JavaScript functionality to capture user input from a form and display it on a grid-container
    This commit introduces JavaScript code to extract user input from a form, creating variables (objects) from the input data and displaying them on a grid-container. This functionality enhances the user experience by dynamically updating the interface with user-generated content. The implementation streamlines the process of capturing and displaying information, contributing to a more interactive and responsive web application.

--- scripts.js
+++ scripts.js
@@ -1,29 +1,48 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, readed) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readed = readed;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+Book.prototype.consoleInfo = function () {
+  console.log(this.title);
+  console.log(this.author);
+  console.log(this.pages);
+  console.log(this.readed);
+};
+
+function addBookToLibrary(title, author, pages, readed) {
+  let newBook = new Book(title, author, pages, readed);
+  myLibrary.push(newBook);
+  newBook.consoleInfo();
 }
 
-// JavaScript to handle star selection
-const ratingInput = document.getElementById("ratingInput");
-const stars = ratingInput.querySelectorAll(".rating span");
+document
+  .getElementById("bookForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting
 
-stars.forEach((star) => {
-  star.addEventListener("click", () => {
-    const rating = parseInt(star.getAttribute("data-value"));
-    // Set the rating value in a hidden input field or handle it as needed
-    console.log("Selected rating:", rating);
-    // Update the appearance of the stars based on the selected rating
-    stars.forEach((s) => {
-      if (parseInt(s.getAttribute("data-value")) <= rating) {
-        s.innerHTML = "&#x2605;"; // filled star
-      } else {
-        s.innerHTML = "&#x2606;"; // empty star
-      }
-    });
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let readed = document.getElementById("readed").checked;
+
+    addBookToLibrary(title, author, pages, readed);
+
+    //create the book divs
+
+    let gridContainer = document.querySelector(".grid-container");
+
+    let newBook = document.createElement("div");
+    newBook.classList.add("book");
+
+    gridContainer.appendChild(newBook);
+
+    let para = document.createElement("p");
+    para.innerHTML = title;
+
+    newBook.appendChild(para);
   });
-});
